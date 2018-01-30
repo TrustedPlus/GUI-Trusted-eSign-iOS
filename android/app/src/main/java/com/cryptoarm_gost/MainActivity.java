0a61cdb2766cea304dc0a60b5b3d5b9c4146f654
IsaacTaitor <isaac220515@gmail.com>
2018-01-30 16:09:12 +0300

    renamed project for android

--- android/app/src/main/java/com/cryptoarm_gost/MainActivity.java
+++ android/app/src/main/java/com/cryptoarm_gost/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativeapp;
+package com.cryptoarm_gost;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ReactNativeApp";
+        return "CryptoARM_GOST";
     }
 }

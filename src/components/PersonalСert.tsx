9f3aad834fad178ccd81e12f1d03c292a716bc82
admin <admin@admins-Mac.local>
2018-02-06 16:46:42 +0300

    changed PropertiesCert

--- src/components/PersonalСert.tsx
+++ src/components/PersonalСert.tsx
@@ -29,10 +29,10 @@ export class PersonalСert extends React.Component<PersonalСertProps> {
           <List>
             <ListMenu title="GisJkh"
              img={require("../../imgs/general/cert2_ok_icon.png" )}
-             rightimg={require("../../imgs/general/key_icon.png" )} note="CRYPTO-PRO Test Center 2" nav={() => navigate("PropertiesCert")}/>
+             rightimg={require("../../imgs/general/key_icon.png" )} note="CRYPTO-PRO Test Center 2" nav={() => null}/>
             <ListMenu title="shesnokov"
              img={require("../../imgs/general/cert2_ok_icon.png")}
-             rightimg={require("../../imgs/general/key_icon.png" )} note='Тестовый УЦ ООО "Крипто Про"' nav={() => null}/>
+             rightimg={require("../../imgs/general/key_icon.png" )} note='Тестовый УЦ ООО "Крипто Про"' nav={() => navigate("PropertiesCert")}/>
             <ListMenu title="serji"
              img={require("../../imgs/general/cert2_bad_icon.png")} note="serji" nav={() => null}/>
           </List>
--- src/components/PropertiesCert.tsx
+++ src/components/PropertiesCert.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Container, ListItem, View, Item, Input, List, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from "native-base";
+import { Container, ListItem, View, Item, Input, List,
+   Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from "native-base";
+import {Image} from "react-native";
 import {Headers} from "./Headers";
-import {ListMenu} from "./ListMenu";
 
 interface PropertiesCertProps {
   navigation: any;
@@ -18,57 +19,66 @@ export class PropertiesCert extends React.Component<PropertiesCertProps> {
     return (
       <Container>
         <Headers title="Свойства сертфиката" src={require("../../imgs/general/back_icon.png")} goBack={() => this.props.navigation.goBack()}/>
+        <Content style={{backgroundColor: "white"}}>
+        <View>
+        <Image style={{height: 70, width: 70, margin: 20}} source={require("../../imgs/general/cert_ok_icon.png")}/>
+        <Text style={{fontSize: 20, position: "absolute", left: 110, top: 20}}>shesnokov</Text>
+        <Text style={{fontSize: 17, color: "grey", position: "absolute", left: 110, top: 50, right: 5}}>Cтатус сертификата:
+          <Text style={{color: "green"}}> действителен</Text>
+        </Text>
+        </View>
         <List>
             <ListItem itemHeader first>
               <Text>Владелец</Text>
             </ListItem>
             <ListItem >
               <Text>Имя:</Text>
-              <Text style={{position: "absolute", right: 5, color: "blue"}}>shesnokov</Text>
+              <Text style={{position: "absolute", width: "60%", right: 5, textAlign: "right", color: "blue"}}>shesnokov</Text>
             </ListItem>
             <ListItem>
               <Text>Email:</Text>
-              <Text style={{position: "absolute", right: 5, color: "blue"}}>shesnokov@gmail.com</Text>
+              <Text style={{position: "absolute", width: "60%", right: 5, textAlign: "right", color: "blue"}}>shesnokov@gmail.com</Text>
             </ListItem>
             <ListItem>
               <Text>Огранизация:</Text>
-              <Text style={{position: "absolute", right: 5, color: "blue"}}>ООО Цифровые технологии</Text>
+              <Text style={{position: "absolute", width: "60%", right: 5, textAlign: "right", color: "blue"}}>ООО Цифровые технологии</Text>
             </ListItem>
             <ListItem last>
               <Text>Страна:</Text>
-              <Text style={{position: "absolute", right: 5, color: "blue"}}>RU</Text>
+              <Text style={{position: "absolute", width: "60%", right: 5, textAlign: "right", color: "blue"}}>RU</Text>
             </ListItem>
             <ListItem itemHeader>
               <Text>Издатель</Text>
             </ListItem>
-            <ListItem>
+            <ListItem last>
               <Text>Имя:</Text>
-              <Text style={{position: "absolute", right: 5, color: "blue"}}>Тестовый УЦ ООО "Крипто Про</Text>
+              <Text style={{position: "absolute", width: "60%", right: 5, textAlign: "right", color: "blue"}}>Тестовый УЦ ООО "Крипто Про"</Text>
             </ListItem>
-            <ListItem itemHeader first>
+            <ListItem itemHeader>
               <Text>Сертфикат</Text>
             </ListItem>
             <ListItem >
               <Text>Серийный номер:</Text>
-              <Text style={{position: "absolute", right: 5, color: "blue"}}>A5 53 TE 3T 43 G4</Text>
+              <Text style={{position: "absolute", width: "60%", right: 5, textAlign: "right", color: "blue"}}>A5 53 TE 3T 43 G4</Text>
             </ListItem>
             <ListItem>
               <Text>Годен до:</Text>
-              <Text style={{position: "absolute", right: 5, color: "blue"}}>24.04.19</Text>
+              <Text style={{position: "absolute", width: "60%", right: 5, textAlign: "right", color: "blue"}}>24.04.19</Text>
             </ListItem>
             <ListItem>
-              <Text>Алгоритм подписи:</Text>
-              <Text style={{position: "absolute", right: 5, color: "blue"}}>ГОСТ Р 34.11/34.10 - 2001</Text>
+              <Text style={{width: "50%"}}>Алгоритм подписи:</Text>
+              <Text style={{position: "absolute", width: "60%", right: 5, textAlign: "right", color: "blue"}}>ГОСТ Р 34.11/34.10 - 2001</Text>
             </ListItem>
             <ListItem>
               <Text>Хэш-алгоритм:</Text>
-              <Text style={{position: "absolute", right: 5, color: "blue"}}>ГОСТ Р 34.11 - 94</Text>
+              <Text style={{position: "absolute", width: "60%", right: 5, textAlign: "right", color: "blue"}}>ГОСТ Р 34.11 - 94</Text>
             </ListItem>
-            <ListItem>
+            <ListItem last>
               <Text>Закрытый ключ:</Text>
-              <Text style={{position: "absolute", right: 5, color: "blue"}}>присутствует</Text>
+              <Text style={{position: "absolute", width: "60%", right: 5, textAlign: "right", color: "blue"}}>присутствует</Text>
             </ListItem>
           </List>
+        </Content>
       </Container>
     );
   }

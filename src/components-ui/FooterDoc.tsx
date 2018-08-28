64c1921d34055d24034986524a55c39e4206d8a6
admin <admin@mbpandrjmalinin.digt.local>
2018-08-28 12:30:06 +0300

    Проверка работы лицензии

--- src/components-ui/FooterDoc.tsx
+++ src/components-ui/FooterDoc.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { Footer, FooterTab, Text, View, Button } from "native-base";
+import { Footer, FooterTab, Text, View, Button, Header, Title } from "native-base";
 import { FooterButton } from "../components/FooterButton";
+import * as Modal from "react-native-modalbox";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
@@ -66,8 +67,16 @@ enum WhatSelected {
 	"other", "allSig", "allEnc", "selected sign", "selected enc", "selected enc and sign"
 }
 
+interface IModals {
+	basicModal: Modal.default;
+}
+
 @(connect(null, mapDispatchToProps) as any)
 export class FooterDoc extends React.Component<FooterDocProps, FooterDocState> {
+
+	private modals: IModals = {
+		basicModal: null
+	};
 
 	constructor(props) {
 		super(props);
@@ -104,88 +113,127 @@ export class FooterDoc extends React.Component<FooterDocProps, FooterDocState> {
 			}
 		}
 		return (
-			<Footer>
-				<FooterTab>
-					{this.state.modalMore
-						? <View style={[styles.modalMore, styles.modalMore6]}>
-							<Footer>
-								<FooterTab>
-									<FooterButton title="Проверить"
-										disabled={mark !== 1}
-										img={require("../../imgs/ios/verify_sign.png")}
-										nav={() => verifySign(files, selectedFilesObject)} />
-									<FooterButton title="Снять"
-										disabled={mark !== 1}
-										img={require("../../imgs/ios/delete_sign.png")}
-										nav={() => { clearAllFilesinAllWorkspace(); UnSignFile(files, selectedFilesObject, () => clearselectedFiles()); }} />
-									<FooterButton title="Свойства"
-										disabled={mark !== 1 || this.props.selectedFiles.arrNum.length !== 1}
-										img={require("../../imgs/ios/view_sign.png")}
-										nav={() => getSignInfo(files, selectedFilesObject, (page, cert) => navigate(page, { cert: cert }))} />
-								</FooterTab>
-							</Footer>
-							<Footer>
-								<FooterTab>
-									<FooterButton title="Раcшифровать"
-										disabled={mark !== 2}
-										img={require("../../imgs/ios/decrypt.png")}
-										style={{ borderTopWidth: 0 }}
-										nav={() => { clearAllFilesinAllWorkspace(); decAssymmetric(files, selectedFilesObject, () => clearselectedFiles()); }} />
-									<FooterButton title="Архивировать"
-										disabled={true}
-										img={require("../../imgs/ios/arhiver.png")}
-										nav={() => null}
-										style={{ borderTopWidth: 0 }} />
-									<FooterButton title="Удалить"
-										img={require("../../imgs/ios/delete.png")}
-										style={{ borderTopWidth: 0 }}
-										nav={() => { clearAllFilesinAllWorkspace(); deleteFile(files, selectedFiles, () => clearselectedFiles()); }} />
-								</FooterTab>
-							</Footer>
+			<>
+				<Footer>
+					<FooterTab>
+						{this.state.modalMore
+							? <View style={[styles.modalMore, styles.modalMore6]}>
+								<Footer>
+									<FooterTab>
+										<FooterButton title="Проверить"
+											disabled={mark !== 1}
+											img={require("../../imgs/ios/verify_sign.png")}
+											nav={() => verifySign(files, selectedFilesObject)} />
+										<FooterButton title="Снять"
+											disabled={mark !== 1}
+											img={require("../../imgs/ios/delete_sign.png")}
+											nav={() => { clearAllFilesinAllWorkspace(); UnSignFile(files, selectedFilesObject, () => clearselectedFiles()); }} />
+										<FooterButton title="Свойства"
+											disabled={mark !== 1 || this.props.selectedFiles.arrNum.length !== 1}
+											img={require("../../imgs/ios/view_sign.png")}
+											nav={() => getSignInfo(files, selectedFilesObject, (page, cert) => navigate(page, { cert: cert }))} />
+									</FooterTab>
+								</Footer>
+								<Footer>
+									<FooterTab>
+										<FooterButton title="Раcшифровать"
+											disabled={mark !== 2}
+											img={require("../../imgs/ios/decrypt.png")}
+											style={{ borderTopWidth: 0 }}
+											nav={() => { clearAllFilesinAllWorkspace(); decAssymmetric(files, selectedFilesObject, () => clearselectedFiles()); }} />
+										<FooterButton title="Архивировать"
+											disabled={true}
+											img={require("../../imgs/ios/arhiver.png")}
+											nav={() => null}
+											style={{ borderTopWidth: 0 }} />
+										<FooterButton title="Удалить"
+											img={require("../../imgs/ios/delete.png")}
+											style={{ borderTopWidth: 0 }}
+											nav={() => this.modals.basicModal.open()} />
+									</FooterTab>
+								</Footer>
+							</View>
+							: null}
+						<FooterButton title="Подписать"
+							img={require("../../imgs/ios/sign.png")}
+							disabled={(mark === 5) || (mark === 4) || (mark === 2)}
+							nav={() => {
+								let selectedFilesForSign = [];
+								for (let i = 0; i < selectedFiles.arrNum.length; i++) {
+									selectedFilesForSign.push(i);
+								}
+								addFilesInWorkspaceSign(files, selectedFiles); navigate("Signature", { selectedFiles: { arrNum: selectedFilesForSign, arrExtension: selectedFiles.arrExtension } });
+								setTimeout(
+									() => {
+										clearselectedFiles();
+										readFiles();
+									},
+									600
+								);
+							}} />
+						<FooterButton title="Зашифровать"
+							img={require("../../imgs/ios/encrypt.png")}
+							disabled={(mark === 5) || (mark === 3) || (mark === 1)}
+							nav={() => {
+								let selectedFilesForEnc = [];
+								for (let i = 0; i < selectedFiles.arrNum.length; i++) {
+									selectedFilesForEnc.push(i);
+								}
+								addFilesInWorkspaceEnc(files, selectedFiles); navigate("Encryption", { selectedFiles: { arrNum: selectedFilesForEnc, arrExtension: selectedFiles.arrExtension } }); setTimeout(
+									() => {
+										clearselectedFiles();
+										readFiles();
+									},
+									600
+								);
+							}} />
+						<FooterButton title="Отправить"
+							disabled={selectedFiles.arrNum.length === 1 ? false : true}
+							img={require("../../imgs/ios/posted.png")}
+							nav={() => uploadFile(files, selectedFiles)} />
+						<FooterButton title="Больше"
+							icon="ios-more"
+							nav={() => this.setState({ modalMore: !this.state.modalMore })} />
+					</FooterTab>
+				</Footer>
+				<Modal
+					ref={ref => this.modals.basicModal = ref}
+					style={[styles.modal, {
+						height: "auto",
+						width: 300,
+						backgroundColor: "white",
+					}]}
+					position={"center"}
+					swipeToClose={false}>
+					<View style={{ width: "100%" }}>
+						<Header
+							style={{ backgroundColor: "#be3817", height: 45.7, paddingTop: 13 }}>
+							<Title>
+								<Text style={{
+									color: "white",
+									fontSize: 15
+								}}>Удаление файлов</Text>
+							</Title>
+						</Header>
+						<View style={{
+							padding: 15, height: 70
+						}}>
+							<Text style={{
+								color: "grey",
+								fontSize: 15
+							}}>Выполнить удаление выбраных файлов с устройства?</Text>
 						</View>
-						: null}
-					<FooterButton title="Подписать"
-						img={require("../../imgs/ios/sign.png")}
-						disabled={(mark === 5) || (mark === 4) || (mark === 2)}
-						nav={() => {
-							let selectedFilesForSign = [];
-							for (let i = 0; i < selectedFiles.arrNum.length; i++) {
-								selectedFilesForSign.push(i);
-							}
-							addFilesInWorkspaceSign(files, selectedFiles); navigate("Signature", { selectedFiles: { arrNum: selectedFilesForSign, arrExtension: selectedFiles.arrExtension } });
-							setTimeout(
-								() => {
-									clearselectedFiles();
-									readFiles();
-								},
-								600
-							);
-						}} />
-					<FooterButton title="Зашифровать"
-						img={require("../../imgs/ios/encrypt.png")}
-						disabled={(mark === 5) || (mark === 3) || (mark === 1)}
-						nav={() => {
-							let selectedFilesForEnc = [];
-							for (let i = 0; i < selectedFiles.arrNum.length; i++) {
-								selectedFilesForEnc.push(i);
-							}
-							addFilesInWorkspaceEnc(files, selectedFiles); navigate("Encryption", { selectedFiles: { arrNum: selectedFilesForEnc, arrExtension: selectedFiles.arrExtension } }); setTimeout(
-								() => {
-									clearselectedFiles();
-									readFiles();
-								},
-								600
-							);
-						}} />
-					<FooterButton title="Отправить"
-						disabled={selectedFiles.arrNum.length === 1 ? false : true}
-						img={require("../../imgs/ios/posted.png")}
-						nav={() => uploadFile(files, selectedFiles)} />
-					<FooterButton title="Больше"
-						icon="ios-more"
-						nav={() => this.setState({ modalMore: !this.state.modalMore })} />
-				</FooterTab>
-			</Footer>
+						<View style={{ display: "flex", flexDirection: "row", flexWrap: "nowrap", justifyContent: "space-around", maxWidth: "100%" }}>
+							<Button transparent style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", width: "50%", borderLeftWidth: 0.25, borderTopWidth: 0.5, borderColor: "grey", borderRadius: 0 }} onPress={() => this.modals.basicModal.close()}>
+								<Text style={{ fontSize: 15, textAlign: "center", color: "grey" }}>Отмена</Text>
+							</Button>
+							<Button transparent style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", width: "50%", borderLeftWidth: 0.25, borderTopWidth: 0.5, borderColor: "grey", borderRadius: 0 }} onPress={() => { this.modals.basicModal.close();  clearAllFilesinAllWorkspace(); deleteFile(files, selectedFiles, () => clearselectedFiles());  }}>
+								<Text style={{ fontSize: 15, textAlign: "center", color: "grey" }}>Да</Text>
+							</Button>
+						</View>
+					</View>
+				</Modal>
+			</>
 		);
 	}
 }
